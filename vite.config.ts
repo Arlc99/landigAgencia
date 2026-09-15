0b4e1bf5e41780cd570f8cc3442099e77c528921
arlc99 <camilo313464@gmail.com>
2026-09-15 15:51:32 -0500

    ci: deploy automático a Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/landigAgencia/',
+  build: {
+    outDir: 'docs',   // 👈 Vite genera en /docs en vez de /dist
+  },
 });
