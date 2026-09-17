a6d829c2f4e4e0025a8bec8c7ee9c46388db3b8f
arlc99 <camilo313464@gmail.com>
2026-09-17 18:26:41 -0500

    borre

--- src/App.tsx
+++ src/App.tsx
@@ -44,8 +44,6 @@ export default function App() {
 
       <Navbar />
       <Hero />
-      <MenuBarStrip />      
-      <InboxMockup />
       <FinalCTA />
       <FeatureTriage />
       <LogoCloud />
