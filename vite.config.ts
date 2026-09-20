3059cfa53565eb4b5caf905a957793960ac79b0f
arlc99 <camilo313464@gmail.com>
2026-09-19 21:09:39 -0500

    desplegandoVercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/landigAgencia/',
-  build: {
-    outDir: 'docs',   // 👈 Vite genera en /docs en vez de /dist
-  },
+  base: '/',
 });
